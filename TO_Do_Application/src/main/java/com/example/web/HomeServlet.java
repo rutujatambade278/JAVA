ec7ddabab523599d826631ed352ef4351228c3b6
Rutuja <rutujatambade278@gmail.com>
2024-12-06 15:15:14 +0530

    crud operaton

---
+++ TO_Do_Application/src/main/java/com/example/web/HomeServlet.java
@@ -0,0 +1,15 @@
+package com.example.web;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getWriter().println("Welcome to the Home Page!");
+    }
+}
---
+++ TO_Do_Application/src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<%@ page language="java" %>
+<html>
+<head><title>Welcome</title></head>
+<body>
+    <h1>Welcome to the TODO Application</h1>
+</body>
+</html>
